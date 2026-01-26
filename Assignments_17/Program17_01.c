b51b8c4fc11e825867e807b8daed2c3a2ad724c9
SomanathGavade172 <somanathgavade2001@gmail.com>
2026-01-26 21:58:02 +0530

    Fix formatting

--- Assignments_17/Program17_01.c
+++ Assignments_17/Program17_01.c
@@ -25,10 +25,12 @@
 //
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+// Function Definition.
 void Patter(int iNo)
 {
     int iCnt = 0;
 
+    // Updator.
     if(iNo < 0)
     {
         iNo = -iNo;
@@ -55,7 +57,8 @@ int main()
     printf("Enter a number : ");
     scanf("%d", &iValue);
 
-    Patter(iValue);
+    Patter(iValue);        //  Function Call.
     
     return 0;
 }
+
