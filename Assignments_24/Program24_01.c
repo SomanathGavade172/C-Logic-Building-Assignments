68154130b844b0b919a6df373f9e3080ae4af7ee
SomanathGavade172 <somanathgavade2001@gmail.com>
2026-02-15 21:20:21 +0530

    Added Advanced Pattern Printing C Assignment

---
+++ Assignments_24/Program24_01.c
@@ -0,0 +1,62 @@
+/* 
+    1. Accept number of rows and number of columns from user and display below pattern.
+
+    Input  : iRow = 4, iCol = 4
+
+    Output :
+                *
+                * *
+                * * *
+                * * * *
+*/
+
+#include<stdio.h>
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name  : Pattern
+//  Description    : This function displays a left-aligned triangle star pattern. For each row, stars are printed equal to the row number.
+//  Input          : Integer (Rows), Integer (Columns)
+//  Output         : NONE (Displays pattern on console)
+//  Author         : Gavade Somanath Vilas
+//  Date           : 15/02/2026
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Function Definition
+void Pattern(int iRow, int iCol)
+{
+    int i = 0, j = 0;
+
+    for(i = 1; i <= iRow; i++)
+    {
+        for(j = 1; j <= i; j++)
+        {
+            printf("*\t");
+        }
+        printf("\n");
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Application Name : Left Triangle Star Pattern Display
+//  Description      : This application accepts number of rows and columns from the user and prints a left-aligned triangle pattern of stars.
+//                     Note: Number of columns is not used in this pattern.
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter number of rows : \n");
+    scanf("%d", &iValue1);
+
+    printf("Enter number of columns : \n");
+    scanf("%d", &iValue2);
+
+    Pattern(iValue1, iValue2);          // Function Call
+
+    return 0;
+}
